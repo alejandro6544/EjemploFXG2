9560cd6f87bc8311b8cb2bd75dd3eb0ac1378a79
bananamit <mitchell.chavarria@uao.edu.co>
2021-08-18 16:02:28 -0500

    cambio 2
    se agrega atributo, getter, setter y constructor

--- src/modelo/Ejemplo1.java
+++ src/modelo/Ejemplo1.java
@@ -12,6 +12,35 @@ package modelo;
 public class Ejemplo1 {
     
     private String nombre1;
+    private String telefono;
+
+    public Ejemplo1() {
+    }
+
+    public Ejemplo1(String nombre1, String telefono) {
+        this.nombre1 = nombre1;
+        this.telefono = telefono;
+    }
+
+    /**
+     * Get the value of telefono
+     *
+     * @return the value of telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * Set the value of telefono
+     *
+     * @param telefono new value of telefono
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    
 
     /**
      * Get the value of nombre1
